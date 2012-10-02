e2e030b7ed7f8b98176abcca3abd1cdc607e14c2
Dimiter 'malkia' Stanev <malkia@gmail.com>
2012-10-01 19:58:50 -0700

    and more fixes

---
+++ luajit32.cmd
@@ -0,0 +1,2 @@
+@SET LUA_PATH=%~dp0\?.lua;%LUA_PATH%
+@"%~dp0bin\Windows\msvcrt\x86\luajit.exe" %*
---
+++ luajit64.cmd
@@ -0,0 +1,2 @@
+@SET LUA_PATH=%~dp0\?.lua;%LUA_PATH%
+@"%~dp0bin\Windows\msvcrt\x64\luajit.exe" %*
